bf382ea00fe49251c3defd5b1d100124d53bfd59
niruhan <niruhanv@gmail.com>
2019-10-29 11:54:04 +0530

    add final script

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,5 @@
 echo "~~~ Starting SI Docker Build ~~~"
-buildNumber=115
+buildNumber=92
 
 PRODUCT_BUILD_STATUS=$(eval "curl -s https://wso2.org/jenkins/job/products/job/streaming-integrator/$buildNumber/api/json | python3 -c \"import sys, json; print(json.load(sys.stdin)['result'])\"")
 
@@ -15,18 +15,16 @@ echo "Wait till artifacts are archived in the product build..."
 
 sleep 120
 
-# DISTRIBUTION_NAME=$(eval "curl -s https://wso2.org/jenkins/job/products/job/streaming-integrator/$buildNumber/api/json | python3 -c \"import sys, json; print(json.load(sys.stdin)['artifacts'][5]['fileName'])\"")
-DISTRIBUTION_NAME=wso2-streaming-integrator-1.0.1-SNAPSHOT.zip #need to automate
+DISTRIBUTION_NAME=$(eval "curl -s https://wso2.org/jenkins/job/products/job/streaming-integrator/$buildNumber/api/json | python3 -c \"import sys, json; print(json.load(sys.stdin)['artifacts'][1]['fileName'])\"")
 echo $DISTRIBUTION_NAME
 TEMP=${DISTRIBUTION_NAME%.zip}
-DISTRIBUTION_VERSION=${TEMP#wso2-streaming-integrator-}
+DISTRIBUTION_VERSION=${TEMP#wso2si-}
 
 rm -fr docker-build
 rm -fr streaming-integrator
 rm -f $DISTRIBUTION_NAME
 
-#wget --progress=dot:mega https://wso2.org/jenkins/job/products/job/micro-integrator/$buildNumber/artifact/distribution/target/$DISTRIBUTION_NAME
-wget https://wso2.org/jenkins/job/products/job/streaming-integrator/$buildNumber/org.wso2.ei\$wso2-streaming-integrator/artifact/org.wso2.ei/wso2-streaming-integrator/$DISTRIBUTION_VERSION/$DISTRIBUTION_NAME
+wget --progress=dot:mega https://wso2.org/jenkins/job/products/job/streaming-integrator/$buildNumber/artifact/target/checkout/modules/distribution/target/$DISTRIBUTION_NAME
 
 git clone --depth=50 https://github.com/wso2/streaming-integrator.git
 mkdir docker-build
@@ -38,8 +36,8 @@ DISTRIBUTION_ROOT=wso2si-${DISTRIBUTION_VERSION}
 
 mv $DISTRIBUTION_ROOT wso2si
 
-docker build --pull -t niruhan/streaming-integrator:$DISTRIBUTION_VERSION .
-docker push niruhan/streaming-integrator:$DISTRIBUTION_VERSION
+docker build --pull -t wso2/streaming-integrator:$DISTRIBUTION_VERSION .
+docker push wso2/streaming-integrator:$DISTRIBUTION_VERSION
 
 cd ..
 
